4a96ae56f795cc7bf969eb90135382483c2deaec
Ruoyu <wuruoyu@me.com>
2021-07-13 20:21:42 -0400

    update extraction.py

--- firmware_analysis/extraction.py
+++ firmware_analysis/extraction.py
@@ -2,35 +2,47 @@ import sys
 import angr
 import struct
 import logging
+import pickle
 from IPython import embed
+
 logging.disable(logging.CRITICAL)
+
+# -----------------------------------------------------------
+# These meta data are required from the result of analysis.py
 
 # dev board
 dev_bin_path = "../firmware/CYPRESS920735Q60EVB-01.bin"
-dev_addr = 0x18741
+dev_func_addr = 0x18741
+dev_start_addr = 0x18741
+dev_opcode_reg = ""
 dev_ogf_reg = "r7"
 dev_ocf_reg = "r6"
 dev_handler_addr = 0x18841
 dev_handler_reg = "r3"
 
-# rasp 
+# rasp
 rasp_bin_path = "../firmware/BCM2837 (Raspberry pi 3).bin"
-rasp_addr = 0x1bf39
+rasp_start_addr = 0x1bf39
+rasp_func_addr = 0x1bf39
+rasp_opcode_reg = ""
 rasp_ogf_reg = "r9"
 rasp_ocf_reg = "r8"
 rasp_handler_addr = 0x1bffd
 rasp_handler_reg = "r3"
 
-# nexus 
+# nexus
 nexus_bin_path = "../firmware/BCM4339 (Nexus 5).bin"
-nexus_addr = 0x99e1
+nexus_start_addr = 0x99e1
+nexus_func_addr = 0x99e1
+nexus_opcode_reg = "r0"
 nexus_ogf_reg = "r0"
 nexus_ocf_reg = "r1"
 nexus_handler_addr = 0x9a13
 nexus_handler_reg = "r0"
-
+# -----------------------------------------------------------
 
 # bluetooth opcode
+# list for each OGF, how many OCF are there
 opcode_list = [(0x1, 0x45), (0x2, 0x11), (0x3, 0x83), (0x4, 0x0f), (0x5, 0x0d),
                (0x6, 0x0a), (0x8, 0x7a), (0x3f, 0xff)]
 
@@ -48,24 +60,26 @@ def get_entry_point(path):
 
 def main(argv):
     if argv[0] == "dev":
+        mode = "DEV"
         bin_path = dev_bin_path
-        start_addr = dev_addr
+        start_addr = dev_start_addr
         ogf_reg = dev_ogf_reg
         ocf_reg = dev_ocf_reg
         handler_addr = dev_handler_addr
         handler_reg = dev_handler_reg
     elif argv[0] == "rasp":
+        mode = "RASP"
         bin_path = rasp_bin_path
-        start_addr = rasp_addr
+        start_addr = rasp_start_addr
         ogf_reg = rasp_ogf_reg
         ocf_reg = rasp_ocf_reg
         handler_addr = rasp_handler_addr
         handler_reg = rasp_handler_reg
     elif argv[0] == "nexus":
+        mode = "NEXUS"
         bin_path = nexus_bin_path
-        start_addr = nexus_addr
-        ogf_reg = nexus_ogf_reg
-        ocf_reg = nexus_ocf_reg
+        start_addr = nexus_func_addr
+        opcode_reg = nexus_opcode_reg
         handler_addr = nexus_handler_addr
         handler_reg = nexus_handler_reg
     else:
@@ -98,8 +112,12 @@ def main(argv):
 
             # setup
             state = proj.factory.blank_state(addr=start_addr)
-            exec("state.regs." + ogf_reg + " = " + str(ogf))
-            exec("state.regs." + ocf_reg + " = " + str(ocf))
+            if mode == "DEV":
+                exec ("state.regs." + ogf_reg + " = " + str(ogf))
+                exec ("state.regs." + ocf_reg + " = " + str(ocf))
+            else:
+                opcode = (ogf << 10) + ocf
+                exec ("state.regs." + opcode_reg + " = " + str(opcode))
             simgr = proj.factory.simgr(state)
 
             # SE
@@ -112,7 +130,8 @@ def main(argv):
 
             # get handler
             for state in simgr.found:
-                exec("handler = state.regs." + handler_reg + ".args[0]")
+                # exec("handler = state.regs." + handler_reg + ".args[0]")
+                handler = (getattr(state.regs, handler_reg)).args[0]
                 if handler not in handler_dic[ogf][ocf]:
                     handler_dic[ogf][ocf].append(handler)
 
@@ -133,6 +152,11 @@ def main(argv):
                     new_list.append(item)
             handler_dic[ogf][ocf] = new_list
             print("(%x, %x) : " % (ogf, ocf), handler_dic[ogf][ocf])
+
+    embed()
+
+    # save to file
+    pickle.dump(handler_dic, open("%s_handler_addr.p", "wb"))
 
 
 if __name__ == "__main__":
